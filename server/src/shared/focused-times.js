ef994c706929b8fa6ec4518a89d094b3b89df7ce
Tomasz Palys <tomasz.palys@swmansion.com>
2021-12-21 18:03:42 +0100

    [server] Delete earliestFocusedTimeConsideredCurrent method
    Summary: We're no longer using it
    
    Test Plan: Flow
    
    Reviewers: atul

--- server/src/shared/focused-times.js
+++ server/src/shared/focused-times.js
@@ -2,13 +2,6 @@
 
 import { pingFrequency } from 'lib/shared/timeouts';
 
-// If the time column for a given entry in the focused table has a time later
-// than this, then that entry is considered current, and consequently the thread
-// in question is considered in focus.
-function earliestFocusedTimeConsideredCurrent(): number {
-  return Date.now() - pingFrequency - 1500;
-}
-
 // If the time column for a given entry in the focused table has a time earlier
 // than this, then that entry is considered expired, and consequently we will
 // remove it from the table in the nightly cronjob.
@@ -16,7 +9,4 @@ function earliestFocusedTimeConsideredExpired(): number {
   return Date.now() - pingFrequency * 2 - 1500;
 }
 
-export {
-  earliestFocusedTimeConsideredCurrent,
-  earliestFocusedTimeConsideredExpired,
-};
+export { earliestFocusedTimeConsideredExpired };
