02cdb03c2f3d77cfad5e9d2d2bcae7769b6faaff
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-23 17:28:19 +0100

    [5/n] [DRAFT] [services] Blob - DB entities - Rev Indx Item

---
+++ services/blob/docker-server/contents/server/src/DatabaseEntities/ReverseIndexItem.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Item.h"
+
+#include <string>
+
+namespace comm {
+namespace network {
+namespace database {
+
+class ReverseIndexItem : public Item {
+
+  std::string reverseIndex;
+  std::string fileHash;
+
+  void validate() const override;
+
+public:
+  static std::string tableName;
+  static const std::string FIELD_REVERSE_INDEX;
+  static const std::string FIELD_FILE_HASH;
+
+  ReverseIndexItem() {
+  }
+  ReverseIndexItem(const std::string reverseIndex, const std::string fileHash);
+  ReverseIndexItem(const AttributeValues &itemFromDB);
+
+  void assignItemFromDatabase(const AttributeValues &itemFromDB);
+
+  const std::string getTableName() const;
+  const std::string getPrimaryKey() const;
+  const std::string getReverseIndex() const;
+  const std::string getFileHash() const;
+};
+
+} // namespace database
+} // namespace network
+} // namespace comm
