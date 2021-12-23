0ce773903f1f9447aa6e09140acd20b8a9b9611e
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-23 09:45:05 +0100

    [8/n] [DRAFT] [services] Blob - tools
    Summary:
    Full stack:
    D2666 D2667 D2668 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: geekbrother, palys-swm
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2670

---
+++ services/blob/docker-server/contents/server/src/Tools.cpp
@@ -0,0 +1,28 @@
+#include "Tools.h"
+
+#include "DatabaseEntitiesTools.h"
+
+#include <chrono>
+
+namespace comm {
+namespace network {
+
+database::S3Path Tools::generateS3Path(
+    const std::string &bucketName,
+    const std::string &fileHash) {
+  // todo this may change
+  return database::S3Path(bucketName, fileHash);
+}
+
+std::string Tools::computeHashForFile(const database::S3Path &s3Path) {
+  return database::BlobItem::FIELD_FILE_HASH; // TODO
+}
+
+long long Tools::getCurrentTimestamp() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch())
+      .count();
+}
+
+} // namespace network
+} // namespace comm
