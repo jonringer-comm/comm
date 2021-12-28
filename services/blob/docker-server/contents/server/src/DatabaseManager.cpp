c95fd3036f72902303bcea4ba231d4348adde054
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:07 +0100

    [12/n] [DRAFT] [services] Blob - db manager for PUT - inner functions
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2674

---
+++ services/blob/docker-server/contents/server/src/DatabaseManager.cpp
@@ -0,0 +1,56 @@
+#include "DatabaseManager.h"
+#include "AwsTools.h"
+#include "Tools.h"
+
+#include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/model/GetItemRequest.h>
+
+#include <iostream>
+
+namespace comm {
+namespace network {
+namespace database {
+
+std::string BlobItem::tableName = BLOB_TABLE_NAME;
+std::string ReverseIndexItem::tableName = REVERSE_INDEX_TABLE_NAME;
+
+DatabaseManager &DatabaseManager::getInstance() {
+  static DatabaseManager instance;
+  return instance;
+}
+
+void DatabaseManager::innerPutItem(
+    std::shared_ptr<Item> item,
+    const Aws::DynamoDB::Model::PutItemRequest &request) {
+  const Aws::DynamoDB::Model::PutItemOutcome outcome =
+      AwsObjectsFactory::getDynamoDBClient()->PutItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+}
+
+std::shared_ptr<Item> DatabaseManager::innerFindItem(
+    Aws::DynamoDB::Model::GetItemRequest &request,
+    const ItemType &itemType) {
+  std::shared_ptr<Item> item = createItemByType(itemType);
+  // Set up the request
+  request.SetTableName(item->getTableName());
+  // Retrieve the item's fields and values
+  const Aws::DynamoDB::Model::GetItemOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->GetItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  const AttributeValues &outcomeItem = outcome.GetResult().GetItem();
+  if (!outcomeItem.size()) {
+    // todo print a fileHash here
+    std::cout << "no item found" << std::endl;
+    return nullptr;
+  }
+  item->assignItemFromDatabase(outcomeItem);
+  return std::move(item);
+}
+
+} // namespace database
+} // namespace network
+} // namespace comm
