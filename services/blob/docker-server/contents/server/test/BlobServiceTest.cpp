ba58d727ad62aa0c04efcef9ae12cb2e919a373d
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:27 +0100

    [21/n] [DRAFT] [services] Blob - tests
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2683

---
+++ services/blob/docker-server/contents/server/test/BlobServiceTest.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "BlobServiceImpl.h"
+
+#include <iostream>
+
+#include <memory>
+#include <random>
+#include <string>
+
+using namespace comm::network;
+
+class BlobServiceTest : public testing::Test {
+protected:
+  std::unique_ptr<BlobServiceImpl> blobService;
+
+  virtual void SetUp() {
+    if (blobService == nullptr) {
+      blobService = std::make_unique<BlobServiceImpl>();
+    }
+  }
+
+  virtual void TearDown() {
+  }
+};
+
+TEST_F(BlobServiceTest, passingTest) {
+  EXPECT_TRUE(true);
+}
