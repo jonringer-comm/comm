88ab8fe9e339fc287acbfa702d9a122a54cc6f19
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:12 +0100

    [14/n] [DRAFT] [services] Blob - implementation - PUT
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2676

---
+++ services/blob/docker-server/contents/server/src/BlobServiceImpl.cpp
@@ -0,0 +1,135 @@
+#include "BlobServiceImpl.h"
+
+#include "AwsStorageManager.h"
+#include "AwsTools.h"
+#include "DatabaseManager.h"
+#include "MultiPartUploader.h"
+#include "Tools.h"
+
+#include <iostream>
+#include <memory>
+
+namespace comm {
+namespace network {
+
+BlobServiceImpl::BlobServiceImpl() {
+  Aws::InitAPI({});
+
+  if (!AwsStorageManager::getInstance()
+           .getBucket(BLOB_BUCKET_NAME)
+           .isAvailable()) {
+    throw std::runtime_error("bucket " + BLOB_BUCKET_NAME + " not available");
+  }
+}
+
+BlobServiceImpl::~BlobServiceImpl() {
+  Aws::ShutdownAPI({});
+}
+
+void BlobServiceImpl::verifyFileHash(
+    const std::string &expectedFileHash,
+    const database::S3Path &s3Path) {
+  const std::string computedFileHash =
+      expectedFileHash; // this->computeHashForFile(*s3Path); // TODO FIX THIS
+  if (expectedFileHash != computedFileHash) {
+    std::string errorMessage = "fileHash mismatch, expected: [";
+    errorMessage +=
+        expectedFileHash + "], computed: [" + computedFileHash + "]";
+    throw std::runtime_error(errorMessage);
+  }
+}
+
+void BlobServiceImpl::assignVariableIfEmpty(
+    const std::string &label,
+    std::string &lvalue,
+    const std::string &rvalue) {
+  if (!lvalue.empty()) {
+    std::string errorMessage = "multiple assignment for variable ";
+    errorMessage += label + " is not allowed";
+    throw std::runtime_error(errorMessage);
+  }
+  lvalue = rvalue;
+}
+
+grpc::Status BlobServiceImpl::Put(
+    grpc::ServerContext *context,
+    grpc::ServerReader<blob::PutRequest> *reader,
+    google::protobuf::Empty *response) {
+  blob::PutRequest request;
+  std::string reverseIndex;
+  std::string receivedFileHash;
+  std::unique_ptr<database::S3Path> s3Path;
+  std::shared_ptr<database::BlobItem> blobItem;
+  std::unique_ptr<MultiPartUploader> uploader;
+  std::string currentChunk;
+  try {
+    while (reader->Read(&request)) {
+      const std::string requestReverseIndex = request.reverseindex();
+      const std::string requestFileHash = request.filehash();
+      const std::string receivedDataChunk = request.datachunk();
+      if (requestReverseIndex.size()) {
+        assignVariableIfEmpty(
+            "reverse index", reverseIndex, requestReverseIndex);
+      } else if (requestFileHash.size()) {
+        assignVariableIfEmpty(
+            "reverse index", receivedFileHash, requestFileHash);
+      } else if (receivedDataChunk.size()) {
+        if (s3Path == nullptr) {
+          throw std::runtime_error(
+              "S3 path or/and MPU has not been created but data "
+              "chunks are being pushed");
+        }
+        if (uploader == nullptr) {
+          uploader = std::make_unique<MultiPartUploader>(
+              AwsObjectsFactory::getS3Client(),
+              BLOB_BUCKET_NAME,
+              s3Path->getObjectName());
+        }
+        currentChunk += receivedDataChunk;
+        if (currentChunk.size() > AWS_MULTIPART_UPLOAD_MINIMUM_CHUNK_SIZE) {
+          uploader->addPart(currentChunk);
+          currentChunk.clear();
+        }
+      }
+      if (reverseIndex.size() && receivedFileHash.size() && s3Path == nullptr) {
+        blobItem = database::DatabaseManager::getInstance().findBlobItem(
+            receivedFileHash);
+        if (blobItem != nullptr) {
+          s3Path = std::make_unique<database::S3Path>(blobItem->getS3Path());
+          break;
+        } else {
+          s3Path = std::make_unique<database::S3Path>(
+              Tools::getInstance().generateS3Path(
+                  BLOB_BUCKET_NAME, receivedFileHash));
+        }
+      }
+    }
+    if (!currentChunk.empty()) {
+      uploader->addPart(currentChunk);
+    }
+    if (blobItem == nullptr) {
+      uploader->finishUpload();
+    }
+    // // compute a fileHash and verify with a provided fileHash
+    this->verifyFileHash(receivedFileHash, *s3Path);
+    // putBlobItem - store a fileHash and a path in the DB
+    if (blobItem == nullptr) {
+      blobItem =
+          std::make_shared<database::BlobItem>(receivedFileHash, *s3Path);
+      database::DatabaseManager::getInstance().putBlobItem(*blobItem);
+    }
+    // putReverseIndexItem - store a reverse index in the DB for a given
+    // fileHash
+    const database::ReverseIndexItem reverseIndexItem(
+        reverseIndex, receivedFileHash);
+    database::DatabaseManager::getInstance().putReverseIndexItem(
+        reverseIndexItem);
+  } catch (std::runtime_error &e) {
+    std::cout << "error: " << e.what() << std::endl;
+    return grpc::Status(grpc::StatusCode::INTERNAL, e.what());
+  }
+  return grpc::Status::OK;
+}
+
+} // namespace network
+} // namespace comm
