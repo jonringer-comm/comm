7a95779d4e8dfd7ecbba7b34f69253d56fbb9d2c
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:18 +0100

    [17/n] [DRAFT] [services] Blob - db manager for REMOVE
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Reviewed By: geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2679

--- services/blob/docker-server/contents/server/src/DatabaseManager.cpp
+++ services/blob/docker-server/contents/server/src/DatabaseManager.cpp
@@ -87,6 +87,77 @@ DatabaseManager::findReverseIndexItemByReverseIndex(
   return std::move(this->innerFindItem<ReverseIndexItem>(request));
 }
 
+std::vector<std::shared_ptr<database::ReverseIndexItem>>
+DatabaseManager::findReverseIndexItemsByHash(const std::string &fileHash) {
+  std::vector<std::shared_ptr<database::ReverseIndexItem>> result;
+
+  Aws::DynamoDB::Model::QueryRequest req;
+  req.SetTableName(ReverseIndexItem::tableName);
+  req.SetKeyConditionExpression("fileHash = :valueToMatch");
+
+  AttributeValues attributeValues;
+  attributeValues.emplace(":valueToMatch", fileHash);
+
+  req.SetExpressionAttributeValues(attributeValues);
+  req.SetIndexName("fileHash-index");
+
+  const Aws::DynamoDB::Model::QueryOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->Query(req);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  const Aws::Vector<AttributeValues> &items = outcome.GetResult().GetItems();
+  for (auto &item : items) {
+    result.push_back(std::make_shared<database::ReverseIndexItem>(item));
+  }
+
+  return result;
+}
+
+void DatabaseManager::removeReverseIndexItem(const std::string &reverseIndex) {
+  std::shared_ptr<database::ReverseIndexItem> item =
+      findReverseIndexItemByReverseIndex(reverseIndex);
+  if (item == nullptr) {
+    throw std::runtime_error(std::string(
+        "no reverse index item found for reverse index " + reverseIndex));
+  }
+  this->innerRemoveItem<ReverseIndexItem>(item->getReverseIndex());
+}
+
+// we should pay attention how this is going to scale
+std::vector<std::string> DatabaseManager::getAllHashes() {
+  Aws::DynamoDB::Model::ScanRequest req;
+  req.SetTableName(BlobItem::tableName);
+
+  std::vector<std::string> result;
+
+  // Perform scan on table
+  const Aws::DynamoDB::Model::ScanOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->Scan(req);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
+  // Reference the retrieved items
+  const Aws::Vector<AttributeValues> &retreivedItems =
+      outcome.GetResult().GetItems();
+  // std::cout << "Number of items retrieved from scan: " << items.size() <<
+  // std::endl;
+  // Iterate each item and print
+  for (const auto &retreivedItem : retreivedItems) {
+    // std::cout << "******************************************************" <<
+    // std::endl; Output each retrieved field and its value
+    BlobItem item(retreivedItem);
+    result.push_back(item.getFileHash());
+    // for (const auto &itemData : item) {
+    //   // std::cout << i.first << ": " << i.second.GetS() << std::endl;
+    //   if (itemData.first == "fileHash") {
+    //     result.push_back(itemData.second.GetS());
+    //   }
+    // }
+  }
+  return result;
+}
+
 } // namespace database
 } // namespace network
 } // namespace comm
--- services/blob/docker-server/contents/server/src/DatabaseManager.h
+++ services/blob/docker-server/contents/server/src/DatabaseManager.h
@@ -5,6 +5,7 @@
 
 #include <aws/core/Aws.h>
 #include <aws/dynamodb/model/AttributeDefinition.h>
+#include <aws/dynamodb/model/DeleteItemRequest.h>
 #include <aws/dynamodb/model/GetItemRequest.h>
 #include <aws/dynamodb/model/PutItemRequest.h>
 
@@ -26,16 +27,26 @@ class DatabaseManager {
   template <typename T>
   std::shared_ptr<T>
   innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request);
+  // the key is either
+  //  - fileHash for blob items
+  //  - reverseIndex for reverse index items
+  template <typename T> void innerRemoveItem(const std::string &key);
 
 public:
   static DatabaseManager &getInstance();
 
   void putBlobItem(const BlobItem &item);
   std::shared_ptr<BlobItem> findBlobItem(const std::string &fileHash);
+  void removeBlobItem(const std::string &fileHash);
 
   void putReverseIndexItem(const ReverseIndexItem &item);
   std::shared_ptr<ReverseIndexItem>
   findReverseIndexItemByReverseIndex(const std::string &reverseIndex);
+  std::vector<std::shared_ptr<database::ReverseIndexItem>>
+  findReverseIndexItemsByHash(const std::string &fileHash);
+  void removeReverseIndexItem(const std::string &reverseIndex);
+
+  std::vector<std::string> getAllHashes();
 };
 
 template <typename T>
@@ -58,6 +69,23 @@ DatabaseManager::innerFindItem(Aws::DynamoDB::Model::GetItemRequest &request) {
   }
   item->assignItemFromDatabase(outcomeItem);
   return std::move(item);
+}
+
+template <typename T>
+void DatabaseManager::innerRemoveItem(const std::string &key) {
+  Aws::DynamoDB::Model::DeleteItemRequest request;
+  // I couldn't avoid DRY here as those requests inherit from DynamoDBRequest
+  // and that class does not have a method `SetTableName`
+  std::shared_ptr<T> item = createItemByType<T>();
+  request.SetTableName(item->getTableName());
+  request.AddKey(
+      item->getPrimaryKey(), Aws::DynamoDB::Model::AttributeValue(key));
+
+  const Aws::DynamoDB::Model::DeleteItemOutcome &outcome =
+      AwsObjectsFactory::getDynamoDBClient()->DeleteItem(request);
+  if (!outcome.IsSuccess()) {
+    throw std::runtime_error(outcome.GetError().GetMessage());
+  }
 }
 
 } // namespace database
