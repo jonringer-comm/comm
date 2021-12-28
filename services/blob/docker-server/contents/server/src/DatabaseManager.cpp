e947d95403bb8ecdff884fe99693b4bb23e38f08
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-12-28 13:36:10 +0100

    [13/n] [DRAFT] [services] Blob - db manager for PUT - public functions
    Summary:
    Full stack:
    D2666 D2667 D2668 D2707 D2708 D2709 D2669 D2670 D2671 D2672 D2673 D2674 D2675 D2676 D2677 D2678 D2679 D2680 D2681 D2682 D2683 D2684 D2685 D2686 D2734 D2735
    
    This introduces the blob service
    
    Test Plan: Please see 2686
    
    Reviewers: palys-swm, geekbrother
    
    Subscribers: ashoat, palys-swm, Adrian, atul, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2675

--- services/blob/docker-server/contents/server/src/DatabaseManager.cpp
+++ services/blob/docker-server/contents/server/src/DatabaseManager.cpp
@@ -2,8 +2,11 @@
 #include "Tools.h"
 
 #include <aws/core/utils/Outcome.h>
+#include <aws/dynamodb/model/QueryRequest.h>
+#include <aws/dynamodb/model/ScanRequest.h>
 
 #include <iostream>
+#include <vector>
 
 namespace comm {
 namespace network {
@@ -22,6 +25,63 @@ void DatabaseManager::innerPutItem(
   if (!outcome.IsSuccess()) {
     throw std::runtime_error(outcome.GetError().GetMessage());
   }
+}
+
+void DatabaseManager::putBlobItem(const BlobItem &item) {
+  Aws::DynamoDB::Model::PutItemRequest request;
+  request.SetTableName(BlobItem::tableName);
+  request.AddItem(
+      BlobItem::FIELD_FILE_HASH,
+      Aws::DynamoDB::Model::AttributeValue(item.getFileHash()));
+  request.AddItem(
+      BlobItem::FIELD_S3_PATH,
+      Aws::DynamoDB::Model::AttributeValue(item.getS3Path().getFullPath()));
+  request.AddItem(
+      BlobItem::FIELD_CREATED,
+      Aws::DynamoDB::Model::AttributeValue(
+          std::to_string(Tools::getInstance().getCurrentTimestamp())));
+
+  this->innerPutItem(std::make_shared<BlobItem>(item), request);
+}
+
+std::shared_ptr<BlobItem>
+DatabaseManager::findBlobItem(const std::string &fileHash) {
+  Aws::DynamoDB::Model::GetItemRequest request;
+  request.AddKey(
+      BlobItem::FIELD_FILE_HASH,
+      Aws::DynamoDB::Model::AttributeValue(fileHash));
+  return std::move(this->innerFindItem<BlobItem>(request));
+}
+
+void DatabaseManager::putReverseIndexItem(const ReverseIndexItem &item) {
+  if (this->findReverseIndexItemByReverseIndex(item.getReverseIndex()) !=
+      nullptr) {
+    std::string errorMessage = "An item for the given reverse index [";
+    errorMessage += item.getReverseIndex();
+    errorMessage += "] already exists";
+    throw std::runtime_error(errorMessage);
+  }
+  Aws::DynamoDB::Model::PutItemRequest request;
+  request.SetTableName(ReverseIndexItem::tableName);
+  request.AddItem(
+      ReverseIndexItem::FIELD_REVERSE_INDEX,
+      Aws::DynamoDB::Model::AttributeValue(item.getReverseIndex()));
+  request.AddItem(
+      ReverseIndexItem::FIELD_FILE_HASH,
+      Aws::DynamoDB::Model::AttributeValue(item.getFileHash()));
+
+  this->innerPutItem(std::make_shared<ReverseIndexItem>(item), request);
+}
+
+std::shared_ptr<ReverseIndexItem>
+DatabaseManager::findReverseIndexItemByReverseIndex(
+    const std::string &reverseIndex) {
+  Aws::DynamoDB::Model::GetItemRequest request;
+  request.AddKey(
+      ReverseIndexItem::FIELD_REVERSE_INDEX,
+      Aws::DynamoDB::Model::AttributeValue(reverseIndex));
+
+  return std::move(this->innerFindItem<ReverseIndexItem>(request));
 }
 
 } // namespace database
