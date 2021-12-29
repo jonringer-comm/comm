b1646000e6eba8d1bd03fa25923cd71915e195fd
Benjamin Schachter <benschac@gmail.com>
2021-12-29 11:55:04 -0500

    [3/n] [landing] add basic team profile block in team section
    Summary: Add team profile block based off https://expo.dev/about. Styling is still a WIP, will get feedback from the team. Layout will be handled in another diff. <add a screen shot>
    
    Test Plan: render team page.
    
    Reviewers: atul
    
    Subscribers:

--- landing/team.react.js
+++ landing/team.react.js
@@ -2,10 +2,34 @@
 
 import * as React from 'react';
 
+import TeamProfile from './team-profile.react.js';
+
 function Team(): React.Node {
   return (
     <div>
-      <h1>Team page</h1>
+      <h2>Team</h2>
+      <section>
+        <TeamProfile
+          name="Ashoat Tevosyan"
+          githubHandle="ashoat"
+          twitterHandle="ashoat"
+          imageUrl="https://avatars.githubusercontent.com/u/863579?v=4"
+        />
+        <TeamProfile
+          name="Atul Madhugiri"
+          githubHandle="atulsmadhugiri"
+          twitterHandle="atuli0"
+          imageUrl="https://avatars.githubusercontent.com/u/32692685?v=4"
+        />
+        <TeamProfile
+          name="Benjamin Schachter"
+          githubHandle="benschac"
+          twitterHandle="benschac"
+          imageUrl="https://avatars.githubusercontent.com/u/2502947?v=4"
+        />
+      </section>
+      <h2>Software Mansion Team</h2>
+      <h2>Investors</h2>
     </div>
   );
 }
