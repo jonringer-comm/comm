5942028e00c7eb4c00afdf60ed1fbde41af68b8e
anand <anandnambakam@gmail.com>
2022-01-04 11:52:48 -0800

    [server] Adding `migrations.js`
    Summary:
    This is the automated MySQL migrations file, which is to be run when server is spun up by `server.js`.
    
    I realize that this file is in the wrong place (I think it belongs in `src/database/`, not `src/scripts/`). I didn't want to move it during review, but will do after.
    
    Test Plan:
    Here are the tests I've run so far:
    
    Tests run so far:
    1. migrations are valid, exits gracefully
    2. `db_version` >= migration index (in other words, the `db_version` is greater than the current migration key)
        1. `continue;`'s until a valid migration index is reached. Exits gracefully if such index exists, and also if it doesn't
    3. `db_version` >= all migration indices (`db_version` is higher than all referenced migrations)
        1. Exits gracefully
    4. `db_version` is not set in `metadata` table
        1. `db_version` is initialized to 0 in `metadata` table. Function continues to one of cases 1 to 3
    5. `db_version` is already set in `metadata` table
        1. `db_version` is not re-initialized to 0. Function continues to one of cases 1 to 3
    6. migration failure, does transaction rollback?
        1. yes
    7. migration success, does transaction commit?
        1. yes
    
    Reviewers: atul
    
    Subscribers: ashoat, palys-swm, Adrian, karol-bisztyga, benschac, boristopalov
    
    Differential Revision: https://phabricator.ashoat.com/D2628

---
+++ server/src/database/migrations.js
@@ -0,0 +1,93 @@
+// @flow
+
+import { dbQuery, SQL } from './database';
+
+const migrations: $ReadOnlyMap<number, () => Promise<void>> = new Map([]);
+
+async function createDbVersionEntry() {
+  const insertQuery = SQL`
+    INSERT INTO metadata (name,data)
+    SELECT *
+    FROM (
+      SELECT 'db_version' AS name,
+      0 AS data
+      )
+    AS TEMP
+    WHERE NOT EXISTS (
+	    SELECT name
+      FROM metadata
+      WHERE name = 'db_version'
+	  ) LIMIT 1
+    `;
+  await dbQuery(insertQuery);
+}
+
+async function getDbVersion() {
+  const versionQuery = SQL`
+    SELECT data
+    FROM metadata
+    WHERE name = 'db_version';
+  `;
+  const [[versionResult]] = await dbQuery(versionQuery);
+  const dbVersion = versionResult.data;
+  console.log(`(node:${process.pid}) db schema version: ${dbVersion}`);
+  return dbVersion;
+}
+
+async function updateDbVersion(dbVersion: number) {
+  const updateQuery = SQL`
+    UPDATE metadata
+    SET data = ${dbVersion}
+    WHERE name = 'db_version';
+      `;
+  await dbQuery(updateQuery);
+}
+
+async function migrate(): Promise<boolean> {
+  let didMigrationsSucceed = false;
+
+  await createDbVersionEntry();
+  const dbVersion = await getDbVersion();
+
+  for (const [idx, value] of migrations.entries()) {
+    if (idx <= dbVersion) {
+      continue;
+    }
+
+    const turnOffAutocommit = SQL`
+      SET autocommit = 0;
+    `;
+    await dbQuery(turnOffAutocommit);
+    const beginTransaction = SQL`
+      START TRANSACTION;
+      `;
+    await dbQuery(beginTransaction);
+
+    try {
+      await value();
+    } catch (e) {
+      console.error(`(node:${process.pid}) migration ${idx} failed.`);
+      console.error(e);
+      const rollbackTransaction = SQL`
+            ROLLBACK;
+        `;
+      await dbQuery(rollbackTransaction);
+      return didMigrationsSucceed;
+    }
+
+    await updateDbVersion(idx);
+    const endTransaction = SQL`
+      COMMIT;
+    `;
+    await dbQuery(endTransaction);
+    console.log(`(node:${process.pid}) migration ${idx} succeeded.`);
+
+    const turnOnAutocommit = SQL`
+      SET autocommit = 1;
+    `;
+    await dbQuery(turnOnAutocommit);
+  }
+  return (didMigrationsSucceed = true);
+}
+
+export default migrate;
